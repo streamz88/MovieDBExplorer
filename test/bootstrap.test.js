2495a885994d8768a023e07ef62795bc6afafcb7
streamz88 <victor.jegede.90@gmail.com>
2017-03-30 11:31:27 +0100

    Setup testing template for the bootstrap module

---
+++ test/bootstrap.test.js
@@ -0,0 +1,12 @@
+import BootstrapModule from  '../src/bootstrap'
+import Chai from 'chai'
+
+describe('BootstrapApp', ()=>{
+
+  describe('#fetchPopularMovies', ()=>{
+    it('should initialise the app with list a list of popular movies', ()=>{
+      Chai.expect(true).to.be.true
+    })
+  })
+
+})
